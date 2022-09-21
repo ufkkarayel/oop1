2cdb1ef17844554144d5ce3cb42792dc191efafc
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2022-09-21 23:42:45 +0300

    Inheriance işlendi.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String mesaj = "Vade oranı";
+
+        Product product1 = new Product();
+        product1.setName("Kahve Makinesi");
+        product1.setDiscount(15);
+        product1.setImageUrl("herresi.png");
+        product1.setUnitPrice(7500);
+        product1.setUnitsInStock(3);
+        // System.out.println(product1.name);
+
+        Product product2 = new Product();
+        product2.name = "Fotoğraf Makinesi";
+        product2.discount = 10;
+        product2.imageUrl = "resim2.png";
+        product2.unitPrice = 8500;
+        product2.unitsInStock = 8;
+
+        Product product3 = new Product();
+        product3.name = "Kamera";
+        product3.discount = 15;
+        product3.imageUrl = "resim3.png";
+        product3.unitPrice = 10000;
+        product3.unitsInStock = 13;
+
+        Product[] products = {product1, product2, product3};
+
+        for (Product product : products
+        ) {
+            System.out.println(product.name);
+
+        }
+        IndividualCustomer individualCustomer = new IndividualCustomer();
+        individualCustomer.setId(1);
+        individualCustomer.setCustomerNumber("1");
+        individualCustomer.setPhone("5559964452");
+        individualCustomer.setFirstName("Ufuk");
+        individualCustomer.setLastName("Karayel");
+
+        CorporateCustomer corporateCustomer = new CorporateCustomer();
+        corporateCustomer.setId(2);
+        corporateCustomer.setCompanyName("Mikropor");
+        corporateCustomer.setPhone("031254521254");
+        corporateCustomer.setTaxNumber("234546421");
+        corporateCustomer.setCustomerNumber("12461354");
+
+        Customer[] customers = {individualCustomer,corporateCustomer};
+
+
+
+
+
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class Product {
+
+//PascalCase ile isimlendirilir.
+
+public class Product {
+    String name;
+    double unitPrice;
+    double discount;
+    String imageUrl;
+    int unitsInStock;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(int unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
 }
